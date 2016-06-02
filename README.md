423797140562c80abbc5e04d0a782947e7bb5c5c
roeis <roeichow@gmail.com>
2016-06-02 14:57:53 +0800

    update

---
+++ README.md
@@ -0,0 +1,32 @@
+# hot-cli
+
+the command line tool for front-end developping.
+
+a koa start-up, a workflow for you to start a node app or single package
+you have a backend ability
+
+## hot
+
+    hot init
+
+    hot build dev
+    hot build yz
+    hot build online
+
+    hot upload dev
+    hot upload yz
+
+    hot serve
+
+## workflow
+
+- init : generate a template structure for you
+- build [env] : build your front-end resource with webpack
+- upload [env] : upload your front-end resouces through ftp
+- serve: start a local server for local developing
+
+## include
+
+
+## about
+@roeichow
--- index.js
+++ index.js
@@ -23,73 +23,72 @@ colors.mode = process.stdout.isTTY ? colors.mode : 'none';
 
 // Setup logging and messaging
 var logMessages = [];
-program.log = (function (debugMode) {
-	return function _log(logEntry, noPrint) {
-		logMessages.push(logEntry);
-		if (!noPrint && debugMode) {
-			console.log('--debug-- '.cyan + logEntry);
-		}
-	};
+program.log = (function(debugMode) {
+    return function _log(logEntry, noPrint) {
+        logMessages.push(logEntry);
+        if (!noPrint && debugMode) {
+            console.log('--debug-- '.cyan + logEntry);
+        }
+    };
 })(process.argv.indexOf('--debug') >= 0);
 
 program.successMessage = function successMessage() {
-	var msg = util.format.apply(this, arguments);
-	program.log('Success: ' + msg, true);
-	console.log(msg.green);
+    var msg = util.format.apply(this, arguments);
+    program.log('Success: ' + msg, true);
+    console.log(msg.green);
 };
 
 program.errorMessage = function errorMessage() {
-	var msg = util.format.apply(this, arguments);
-	program.log('Error: ' + msg, true);
-	console.log(msg.red);
+    var msg = util.format.apply(this, arguments);
+    program.log('Error: ' + msg, true);
+    console.log(msg.red);
 };
 
 program.handleError = function handleError(err, exitCode) {
-	if (err) {
-		if (err.message) {
-			program.errorMessage(err.message);
-		} else {
-			program.errorMessage(err);
-		}
-	}
+    if (err) {
+        if (err.message) {
+            program.errorMessage(err.message);
+        } else {
+            program.errorMessage(err);
+        }
+    }
 
-	console.log('For more information see: ' + program.LOG_PATH);
+    console.log('For more information see: ' + program.LOG_PATH);
 
-	fs.writeFileSync(program.LOG_PATH, logMessages.join('\n') + '\n');
+    fs.writeFileSync(program.LOG_PATH, logMessages.join('\n') + '\n');
 
-	process.exit(exitCode || 1);
+    process.exit(exitCode || 1);
 };
 
 // Create request wrapper
-program.request = function (opts, next) {
-  if (program.debug) {
-    program.log('REQUEST: '.bold + JSON.stringify(opts, null, 2));
-  } else {
-  	program.log(opts.uri);
-  }
-  status.start();
-  return request(opts, function (err, res, body) {
-  	status.stop();
-    if (err) {
-      if (program.debug) {
-        program.errorMessage(err.message);
-      }
-      return next(err, res, body);
+program.request = function(opts, next) {
+    if (program.debug) {
+        program.log('REQUEST: '.bold + JSON.stringify(opts, null, 2));
+    } else {
+        program.log(opts.uri);
     }
-    else {
-      if (program.debug) {
-        program.log('RESPONSE: '.bold + JSON.stringify(res.headers, null, 2));
-        program.log('BODY: '.bold + JSON.stringify(res.body, null, 2));
-      }
-      return next(err, res, body);
-    }
-  });
+    status.start();
+    return request(opts, function(err, res, body) {
+        status.stop();
+        if (err) {
+            if (program.debug) {
+                program.errorMessage(err.message);
+            }
+            return next(err, res, body);
+        } else {
+            if (program.debug) {
+                program.log('RESPONSE: '.bold + JSON.stringify(res.headers, null, 2));
+                program.log('BODY: '.bold + JSON.stringify(res.body, null, 2));
+            }
+            return next(err, res, body);
+        }
+    });
 };
 
 
 program.on('*', function() {
-	console.log('Unknown Command: ' + program.args.join(' '));
-	program.help();
+    console.log('Unknown Command: ' + program.args.join(' '));
+    program.help();
 });
 
 // Process Commands
